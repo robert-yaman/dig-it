246fd382e8bf8dc48197a9f1a484d4fb5064bdcd
robert-yaman <robert.e.yaman@gmail.com>
2015-08-12 13:44:55 -0700

    Add config option to application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Capstone
   class Application < Rails::Application
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
