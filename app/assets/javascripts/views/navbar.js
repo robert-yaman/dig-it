ce09401294cf259927c2000c135295f4043a6ea2
robert-yaman <robert.e.yaman@gmail.com>
2015-08-12 14:07:48 -0700

    fireSearch function in the navbar view

--- app/assets/javascripts/views/navbar.js
+++ app/assets/javascripts/views/navbar.js
@@ -5,6 +5,10 @@ Capstone.Views.Navbar = Backbone.View.extend({
   id: "navbar",
   className: "navbar navbar-default navbar-fixed-top",
 
+  events: {
+    "click button.search" : "fireSearch"
+  },
+
   initialize: function () {
     this.listenTo(Capstone.currentUser, "sync", this.render);
   },
@@ -13,5 +17,14 @@ Capstone.Views.Navbar = Backbone.View.extend({
     var content = this.template();
     this.$el.html(content);
     return this;
+  },
+
+  fireSearch: function(event) {
+    event.preventDefault();
+    var searchString = $("#search-bar").val();
+    if (searchString === "") return;
+
+    Backbone.history.navigate("#search/" + searchString, {trigger: true}) ///need to encode
+    var searchString = $("#search-bar").val("");
   }
 });
