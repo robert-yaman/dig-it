adf5c621b84591dbfbcb94246c204c7aff4447ef
robert-yaman <robert.e.yaman@gmail.com>
2015-08-14 18:09:04 -0700

    typo in previous

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = false
 end
