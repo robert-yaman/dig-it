ae256b08a53f47e215b605b8667b323ff7526fef
robert-yaman <robert.e.yaman@gmail.com>
2015-08-16 22:40:03 -0700

    Style user profile

--- app/assets/javascripts/views/user_info.js
+++ app/assets/javascripts/views/user_info.js
@@ -1,11 +1,10 @@
 Capstone.Views.UserInfo = Backbone.CompositeView.extend({
   template: JST["user_info"],
-  className: "user-info",
 
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
 
-    this.addSubview(".most-popular-song span", new Capstone.Views.UserListItemSongItem({model: this.model.mostPopularSong() }))
+    this.addSubview(".most-popular-song .song-item", new Capstone.Views.UserListItemSongItem({model: this.model.mostPopularSong() }))
   },
 
   render: function () {
--- app/assets/javascripts/views/user_list_item_song_item.js
+++ app/assets/javascripts/views/user_list_item_song_item.js
@@ -12,6 +12,7 @@ Capstone.Views.UserListItemSongItem = Backbone.View.extend({
   initialize: function () {
     //change listener is for most popular song on user show page
     this.listenTo(this.model, "change", this.render)
+    
     this.model.on("sync", function(model, resp, options) {
       if (options.silent) return
       this.render;
