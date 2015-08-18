f43ac0e0a476711ccf7332b4d8db031f947cc4e8
robert-yaman <robert.e.yaman@gmail.com>
2015-08-17 22:07:38 -0700

    Error handling for landing page

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,7 +6,7 @@ class Api::UsersController < ApplicationController
       login!(@user)
       render json: @user, status: 200
     else
-      render json: @user, status: 422
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
