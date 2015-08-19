1269093abc67718acfe91836a9bca03fcf293025
robert-yaman <robert.e.yaman@gmail.com>
2015-08-19 15:18:03 -0700

    Modal search view for other users following

--- app/assets/javascripts/views/modal_search_view.js
+++ app/assets/javascripts/views/modal_search_view.js
@@ -3,6 +3,10 @@ Capstone.Views.ModalSearchView = Backbone.CompositeView.extend({
   className: "col-xs-6 col-xs-offset-3",
   id: "modal-search-view",
 
+  events: {
+    "keyup .search-bar input" : "dynamicSearch"
+  },
+
   configSongList: function () {
     if (this.subviews(".modal-search-list").first()) {
       this.removeSubview(".modal-search-list", this.subviews(".modal-search-list").first());
@@ -17,6 +21,26 @@ Capstone.Views.ModalSearchView = Backbone.CompositeView.extend({
     }
     var view = new Capstone.Views.UserList({collection: this.collection});
     this.addSubview(".modal-search-list", view);
+  },
+
+  dynamicSearch: function () { //this seems pretty inefficient
+    var queryString = this.$(".search-bar input").val()
+    var newModels = this.collection.filter(function(model) {
+      return model.escape("name").toLowerCase().match(queryString.toLowerCase()) ||
+             model.escape("username").toLowerCase().match(queryString.toLowerCase());
+    });
+
+    if (this.collection.constructor === Capstone.Collections.Users){
+      var newCollection = new Capstone.Collections.Users(newModels)
+      this.removeSubview(".modal-search-list", this.subviews(".modal-search-list").first());
+      var view = new Capstone.Views.UserList({collection: newCollection});
+      this.addSubview(".modal-search-list", view);
+    } else if (this.collection.constructor === Capstone.Collections.Songs) {
+      var newCollection = new Capstone.Collections.Users(newModels)
+      this.removeSubview(".modal-search-list", this.subviews(".modal-search-list").first());
+      var view = new Capstone.Views.SongList({collection: newCollection});
+      this.addSubview(".modal-search-list", view);
+    }
   },
 
   render: function () {
